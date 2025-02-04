51978bef57694c5683e70b1654b3afb65985e059
Ian W <49083526+Landaman@users.noreply.github.com>
2025-02-04 00:50:39 -0500

    Simplify zoxide-fzf-tmux-session setup

--- zoxide-fzf-tmux-session.nix
+++ zoxide-fzf-tmux-session.nix
@@ -2,21 +2,46 @@
   pkgs,
 }:
 with pkgs;
-stdenvNoCC.mkDerivation {
-  pname = "zoxide-fzf-tmux-session";
-  version = "1";
-  src = ./.;
+writeShellApplication {
+  name = "tmux-session";
 
-  dontBuild = true;
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [
+  runtimeInputs = [
     tmux
     fzf
     zoxide
   ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp ./zoxide-fzf-tmux-session $out/bin/tmux-session
+  text = ''
+    #!/usr/bin/env bash
+
+    if [[ $# -eq 1 ]]; then
+        selected=$1
+    else
+        selected=$(zoxide query --list | fzf || true)
+    fi
+
+    if [[ -z $selected ]]; then
+        exit 0
+    fi
+
+    selected_name=$(basename "$selected" | tr ." " _)
+    tmux_running=$(pgrep tmux || true) 
+
+    if [[ -z ''${TMUX:-} ]]; then
+        if [[ -z $tmux_running ]]; then
+            tmux new-session -s "$selected_name" -c "$selected"
+            exit 0;
+        elif ! tmux has-session -t="$selected_name" 2> /dev/null; then
+            tmux new-session -ds "$selected_name" -c "$selected"
+        fi
+
+        tmux attach-session -t "$selected_name"
+    else
+        if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+            tmux new-session -ds "$selected_name" -c "$selected"
+        fi
+
+        tmux switch-client -t "$selected_name"
+    fi
   '';
 }
