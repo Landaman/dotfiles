b3444d56c915c6c0fbb5d089b805d54a9cab3b36
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-11-12 22:01:50 -0600

    Update aerospace config to be set through Home Manager

--- home.nix
+++ home.nix
@@ -91,104 +91,6 @@ in
 
   home.file.".p10k.zsh".source = ./.p10k.zsh;
 
-  home.file.".aerospace.toml".text = ''
-    start-at-login = true
-
-    accordion-padding = 30
-
-    on-focused-monitor-changed = ['move-mouse monitor-lazy-center']
-
-    ${builtins.concatStringsSep "\n" (
-      builtins.map (appId: ''
-        [[on-window-detected]]
-         if.app-id="${appId}"
-         run= [
-           "layout floating",
-         ] 
-      '') floatingApps
-    )}
-
-    [mode.main.binding]
-        alt-slash = 'layout tiles horizontal vertical'
-        alt-comma = 'layout accordion horizontal vertical'
-
-        alt-enter = ''''exec-and-forget osascript -e '
-        if application "Ghostty" is running then
-            tell application "System Events"
-                tell application "Ghostty" to activate
-                keystroke "n" using {command down}
-            end tell
-        else
-            tell application "Ghostty" to activate
-        end if
-        '
-        ''''
-
-        alt-shift-enter = ''''exec-and-forget osascript -e '
-        if application "Safari" is running then
-            tell application "Safari" to make new document
-        else
-            tell application "Safari" to activate
-        end if
-        '
-        ''''
-
-        alt-h = 'focus left'
-        alt-j = 'focus down'
-        alt-k = 'focus up'
-        alt-l = 'focus right'
-
-        alt-shift-h = 'move left'
-        alt-shift-j = 'move down'
-        alt-shift-k = 'move up'
-        alt-shift-l = 'move right'
-
-        alt-minus = 'resize smart -50'
-        alt-equal = 'resize smart +50'
-        alt-0 = 'balance-sizes'
-
-        alt-1 = 'workspace 1'
-        alt-2 = 'workspace 2'
-        alt-3 = 'workspace 3'
-        alt-4 = 'workspace 4'
-        alt-5 = 'workspace 5'
-        alt-6 = 'workspace 6'
-        alt-7 = 'workspace 7'
-        alt-8 = 'workspace 8'
-        alt-9 = 'workspace 9'
-
-        alt-shift-1 = 'move-node-to-workspace 1'
-        alt-shift-2 = 'move-node-to-workspace 2'
-        alt-shift-3 = 'move-node-to-workspace 3'
-        alt-shift-4 = 'move-node-to-workspace 4'
-        alt-shift-5 = 'move-node-to-workspace 5'
-        alt-shift-6 = 'move-node-to-workspace 6'
-        alt-shift-7 = 'move-node-to-workspace 7'
-        alt-shift-8 = 'move-node-to-workspace 8'
-        alt-shift-9 = 'move-node-to-workspace 9'
-
-        alt-a = 'workspace-back-and-forth'
-
-        alt-tab = 'focus-monitor --wrap-around next'
-        alt-shift-tab = 'move-workspace-to-monitor --wrap-around next'
-
-        alt-shift-semicolon = 'mode service'
-
-        alt-p = 'workspace prev --wrap-around'
-        alt-n = 'workspace next --wrap-around'
-
-    [mode.service.binding]
-        esc = ['reload-config', 'mode main']
-        r = ['flatten-workspace-tree', 'mode main']
-        f = ['layout floating tiling', 'mode main']
-        backspace = ['close-all-windows-but-current', 'mode main']
-
-        alt-shift-h = ['join-with left', 'mode main']
-        alt-shift-j = ['join-with down', 'mode main']
-        alt-shift-k = ['join-with up', 'mode main']
-        alt-shift-l = ['join-with right', 'mode main']
-  '';
-
   # ZSH config
   programs.zsh = {
     enable = true;
@@ -421,11 +323,141 @@ in
     };
   };
 
+  programs.aerospace = {
+    enable = true;
+    launchd = {
+      enable = true;
+      keepAlive = true;
+    };
+    userSettings = {
+      accordion-padding = 30;
+      gaps = {
+        outer.left = 0;
+        outer.bottom = 0;
+        outer.top = 0;
+        outer.right = 0;
+      };
+
+      on-focused-monitor-changed = [ "move-mouse monitor-lazy-center" ];
+      on-window-detected = (
+        builtins.map (appId: {
+          "if" = {
+            app-id = appId;
+          };
+          run = [ "layout floating" ];
+        }) floatingApps
+      );
+
+      mode.main.binding = {
+        alt-slash = "layout tiles horizontal vertical";
+        alt-comma = "layout accordion horizontal vertical";
+
+        alt-enter = "exec-and-forget osascript -e '
+          if application \"Ghostty\" is running then
+            tell application \"System Events\"
+              tell application \"Ghostty\" to activate
+              keystroke \"n\" using {command down}
+            end tell
+          else
+            tell application \"Ghostty\" to activate
+          end if
+        '
+        ";
+        alt-shift-enter = "exec-and-forget osascript -e '
+          if application \"Safari\" is running then
+            tell application \"Safari\" to make new document
+          else
+            tell application \"Safari\" to activate
+          end if
+        '
+        ";
+
+        alt-h = "focus left";
+        alt-j = "focus down";
+        alt-k = "focus up";
+        alt-l = "focus right";
+
+        alt-shift-h = "move left";
+        alt-shift-j = "move down";
+        alt-shift-k = "move up";
+        alt-shift-l = "move right";
+
+        alt-minus = "resize smart -50";
+        alt-equal = "resize smart +50";
+        alt-0 = "balance-sizes";
+
+        alt-1 = "workspace 1";
+        alt-2 = "workspace 2";
+        alt-3 = "workspace 3";
+        alt-4 = "workspace 4";
+        alt-5 = "workspace 5";
+        alt-6 = "workspace 6";
+        alt-7 = "workspace 7";
+        alt-8 = "workspace 8";
+        alt-9 = "workspace 9";
+
+        alt-shift-1 = "move-node-to-workspace 1";
+        alt-shift-2 = "move-node-to-workspace 2";
+        alt-shift-3 = "move-node-to-workspace 3";
+        alt-shift-4 = "move-node-to-workspace 4";
+        alt-shift-5 = "move-node-to-workspace 5";
+        alt-shift-6 = "move-node-to-workspace 6";
+        alt-shift-7 = "move-node-to-workspace 7";
+        alt-shift-8 = "move-node-to-workspace 8";
+        alt-shift-9 = "move-node-to-workspace 9";
+
+        alt-a = "workspace-back-and-forth";
+
+        alt-tab = "focus-monitor --wrap-around next";
+        alt-shift-tab = "move-workspace-to-monitor --wrap-around next";
+
+        alt-shift-semicolon = "mode service";
+
+        alt-p = "workspace prev --wrap-around";
+        alt-n = "workspace next --wrap-around";
+      };
+      mode.service.binding = {
+        esc = [
+          "reload-config"
+          "mode main"
+        ];
+        r = [
+          "flatten-workspace-tree"
+          "mode main"
+        ];
+        f = [
+          "layout floating tiling"
+          "mode main"
+        ];
+        backspace = [
+          "close-all-windows-but-current"
+          "mode main"
+        ];
+
+        alt-shift-h = [
+          "join-with left"
+          "mode main"
+        ];
+        alt-shift-j = [
+          "join-with down"
+          "mode main"
+        ];
+        alt-shift-k = [
+          "join-with up"
+          "mode main"
+        ];
+        alt-shift-l = [
+          "join-with right"
+          "mode main"
+        ];
+      };
+    };
+  };
+
   # Packages available to only this user
   home.packages = with pkgs; [
     awscli2
     terraform
-    aerospace
     bruno
     wireshark
     bitwarden-cli
