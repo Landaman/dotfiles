06a4e65cb1d4dc65a8ac1ac3e27621521951f530
Ian Wright <49083526+Landaman@users.noreply.github.com>
2026-04-25 13:54:37 -0500

    Move Aerospace floating windows config to Aerospace

--- modules/window.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-{
-  options.window = {
-    floatingApps = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      description = "Apps that should always open in floating window mode";
-    };
-  };
-}
